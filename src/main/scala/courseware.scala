24fcc119049cc103cf6bd3ff0bdd17c870095f36
maxwageneder <maximilian.wageneder@edu.fh-joanneum.at>
2016-01-18 17:39:38 +0100

    some Changes regarding SQL Connection

--- src/main/scala/courseware.scala
+++ src/main/scala/courseware.scala
@@ -1,7 +1,33 @@
 /**
-  * Created by Thomas on 06.12.2015.
-  * looki looki by Max on 06.12.2015
-  */
+ * Created by Thomas on 06.12.2015.
+ * looki looki by Max on 06.12.2015
+ */
+
+import fhj.swengb.Db
+
+import scala.util.Try
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
+
+/*
+ * Driver d = (Driver)Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver").newInstance();
+ * con = DriverManager.getConnection("jdbc:microsoft:sqlserver://localhost:1433", "userName", "password");
+ */
+
+
+lazy val maybeConnection: Try[Connection] =
+  Try(DriverManager.getConnection("jdbc:sqlite::memory:"))
+
 object Courseware {
+
+/*
+  def main(args: Array[String]) {
+    for {con <- Db.maybeConnection
+         Person  _ = .reTable(con.createStatement())
+         _ = Students.sortedStudents.map(toDb(con)(_))
+         s <- Person.fromDb(queryAll(con))} {
+      println(s)
+    }
+  }
+*/
 
 }
