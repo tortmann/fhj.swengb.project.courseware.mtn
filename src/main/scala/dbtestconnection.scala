45a7421cc8da381a8ce494ae671ad2a4f1086835
maxwageneder <maximilian.wageneder@edu.fh-joanneum.at>
2016-01-24 21:38:25 +0100

    db connection doesnt work out as expected .. different versions but the drivers doesnt work..

--- src/main/scala/dbtestconnection.scala
+++ src/main/scala/dbtestconnection.scala
@@ -13,12 +13,17 @@ object dbtestconnection {
 
   def main(args: Array[String]) {
     // parameters for the connection
-    // val driver = "com.microsoft.jdbc.sqlserver.SQLServerDriver"
+    val driver = "com.microsoft.jdbc.sqlserver.SQLServerDriver"
+    //val driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+
+
     // val url = "jdbc:microsoft:sqlserver://10.25.2.143:1433;DatabaseName=daent_g1"
+    val url="jdbc:microsoft:sqlserver://10.25.2.143:1433;databaseName=daent_gr1;"
+    //val url = "jdbc:msql://10.25.2.143:1114/daent_gr1"
+
     val username = "wagm"
     val password = "wagenede14"
 
-    val url = "jdbc:msql://10.25.2.143:1114/daent_gr1"
 
 
     // Connection --> NULL setzen
@@ -26,12 +31,13 @@ object dbtestconnection {
 
     try {
       // connection connecten
-      Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver");
+      Class.forName(driver);
       connection = DriverManager.getConnection(url, username, password)
 
+       //connection = DriverManager.getConnection("jdbc:sqlserver://10.25.2.143:1433;" +
+       //  "databaseName=daent_g1;user=wagm;password=wagenede14");
 
-      // connection = DriverManager.getConnection("jdbc:sqlserver:10.25.2.143:1433;" +
-      //   "databaseName=daent_g1;user=wagm;password=wagenede14");
+
 
       // "SQL Statement"
       val statement = connection.createStatement()
@@ -39,7 +45,7 @@ object dbtestconnection {
       while ( resultSet.next() ) {
         val firstname = resultSet.getString("firstname")
         val lastname = resultSet.getString("lastname")
-        println("firstname, lastname = " + firstname + ", " + lastname)
+        println("firstname, lastname = " + firstname + " " + lastname)
       }
     } catch {
       case e => e.printStackTrace
