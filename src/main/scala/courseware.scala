0955a7ff81bdecd6cd21c7a92b16534fcbb80d4f
maxwageneder <maximilian.wageneder@edu.fh-joanneum.at>
2016-01-25 17:05:59 +0100

    dbconnection works

--- src/main/scala/courseware.scala
+++ src/main/scala/courseware.scala
@@ -1,29 +1,78 @@
 import scala.util.Try
 import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
-/*
- * Driver d = (Driver)Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver").newInstance();
- * con = DriverManager.getConnection("jdbc:microsoft:sqlserver://localhost:1433", "userName", "password");
- */
-
-/*
- val maybeConnection: Try[Connection] =
-  Try(DriverManager.getConnection("jdbc:microsoft:sqlserver://10.25.2.143", "wagm", "wagenede14"))
 
 
+
+object Db {
+  /**
+   * A marker interface for datastructures which should be persisted to a jdbc database.
+   *
+   * @tparam T the type to be persisted / loaded
+   */
+  trait DbEntity[T] {
+    /**
+     * Saves given type to the database.
+     *
+     * @param c
+     * @param t
+     * @return
+     */
+    def toDb(c: Connection)(t: T): Int
+    /**
+     * Given the resultset, it fetches its rows and converts them into instances of T
+     *
+     * @param rs
+     * @return
+     */
+    def fromDb(rs: ResultSet): List[T]
+    /**
+     * Queries the database
+     *
+     * @param con
+     * @param query
+     * @return
+     */
+    def query(con: Connection)(query: String): ResultSet = {
+      con.createStatement().executeQuery(query)
+    }
+    /**
+     * sql code for inserting an entity.
+     */
+    def insertSql: String
+
+  }
+}
 
 object Courseware {
 
-/*
+
   def main(args: Array[String]) {
-    for {con <- Db.maybeConnection
-         Person  _ = .reTable(con.createStatement())
-         _ = Students.sortedStudents.map(toDb(con)(_))
-         s <- Person.fromDb(queryAll(con))} {
-      println(s)
+
+
+    // parameters for the connection
+    val driver = "com.microsoft.jdbc.sqlserver.SQLServerDriver"
+    val url="jdbc:microsoft:sqlserver://10.25.2.143:1433;databaseName=daent_g1;"
+    val username = "wagm"
+    val password = "wagenede14"
+
+
+
+    // Connection --> NULL setzen
+    var connection:Connection = null
+
+    Class.forName(driver)
+    connection = DriverManager.getConnection(url, username, password)
+
+
+    for (t <- Teacher.fromDb(Teacher.queryAll(connection))) {
+      println(t)
     }
+
+    for (t <- Student.fromDb(Student.queryAll(connection))) {
+      println(t)
+    }
+
   }
-*/
 
 }
-*/
